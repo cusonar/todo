1386a99b804c5fa89b0021a2cad413505b26775a
cusonar <cusonar@192.168.0.11>
2018-09-07 23:45:15 +0900

    FrontEnd 추

---
+++ src/main/java/com/cusonar/todo/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.cusonar.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("")
+	public String index() {
+		return "/index.html";
+	}
+}
